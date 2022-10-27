1c5a74b8c66e9564d759c234dcfc70779e488555
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-10-27 11:30:44 +0700

    js day2 basic

--- book_purchase.js
+++ book_purchase.js
@@ -1,5 +1,4 @@
-function priceCalculator(book, discount, tax) {
-    let result = 0;
+function priceCalculator(book, discount, tax, stock, amount_purchase) {
     if (book && book.price) {
         const print = book.printing_status;
         const price = book.price;
@@ -8,18 +7,34 @@ function priceCalculator(book, discount, tax) {
         const priceAfterDiscount = price - totalDiscount;
         const totalTax = priceAfterDiscount * (tax/100);
         const priceAfterTax = priceAfterDiscount + totalTax;
-
+        let total = 0;
+        let total_book = 0;
+        for (let i = 0; i < amount_purchase; i++) {
+            stock -= 1;
+            total_book += 1;
+            total = total + priceAfterTax;
+            if (stock == 0) {
+                break;
+            }
+        }
         if (print == true) {
-            console.log('Book Title', title);
-            console.log('Original Price', price);
-            console.log('Discount Amount', totalDiscount);
-            console.log('Price After Discount', priceAfterDiscount);
-            console.log('Tax Amount', totalTax);
-            console.log('Price After Tax', priceAfterTax);
-            console.log('TOTAL', priceAfterTax);
+            console.log('Book Title\t\t', title);
+            console.log('Original Price\t\t', price);
+            console.log('Discount Amount\t\t', totalDiscount);
+            console.log('Price After Discount\t', priceAfterDiscount);
+            console.log('Tax Amount\t\t', totalTax);
+            console.log('Price After Tax\t\t', priceAfterTax);
+            console.log(total_book, 'Book, TOTAL \t\t', total);
+            console.log('=======================================');
+            if (stock == 0) {
+                console.log(title, 'Book is Already Out of Stock');
+                console.log(title, 'Book CANNOT be Purchased Again')
+            } else{
+                console.log(title, 'Book CAN be Purchased Again')
+            }
+            console.log('STOCK:', stock);
         }
     }
-    return result;
 }
 
-let finalPrice = priceCalculator({title: 'Harpot', price:10000, printing_status: true}, 10, 10)
+let finalPrice = priceCalculator({title: 'Harpot', price:10000, printing_status: true}, 10, 10, 10, 5)
