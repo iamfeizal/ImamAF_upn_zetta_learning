f09fd080acfdfe0c2ef6bcee51a11b64ffd84770
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-10-28 21:03:09 +0700

    compile ts ke js

---
+++ text_concat.js
@@ -0,0 +1,7 @@
+function concat(arr) {
+    return arr.join(" ");
+}
+var text1 = [1, 'data', '3', 'result'];
+var text2 = ['Bejo', 'has', 4, 'sport', 'car'];
+console.log(concat(text1));
+console.log(concat(text2));
