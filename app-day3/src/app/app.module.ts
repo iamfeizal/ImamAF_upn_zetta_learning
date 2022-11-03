6a5d9847dd9fe1d8bd53696db31f8830469c01da
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-11-03 18:07:09 +0700

    fix bug angular day 3

---
+++ app-day3/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app-day3/src/app/card/card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  newTaskName = '';
+  newTaskContent = '';
+  @Output() taskCreated = new EventEmitter<{taskName: string,taskContent: string}>();
+  @Output()groupTaskCreated = new EventEmitter<{taskName: string,taskContent: string}>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddTask() {
+    this.taskCreated.emit({taskName:this.newTaskName,taskContent:this.newTaskContent})
+  }
+
+  onAddGroupTask() {
+    this.groupTaskCreated.emit({taskName:this.newTaskName,taskContent:this.newTaskContent})
+  }
+}
