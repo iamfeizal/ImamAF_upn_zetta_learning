94524854663bf1b3064054277853958c9acfe043
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-11-21 22:50:16 +0700

    angular day11 basic

--- api-app/src/app/app.component.ts
+++ api-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { DataService } from './service/data.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api-app';
+
 }
--- api-app/src/app/app.module.ts
+++ api-app/src/app/app.module.ts
@@ -1,16 +1,44 @@
+import { DataService } from './service/data.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { EditDataComponent } from './edit/edit-data/edit-data.component';
+import { ListDataComponent } from './list/list-data/list-data.component';
+import { SearchPipe } from './search.pipe';
+import { AddDataComponent } from './add/add-data/add-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditDataComponent,
+    ListDataComponent,
+    SearchPipe,
+    AddDataComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
